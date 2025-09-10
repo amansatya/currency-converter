a0303322805837e898b7b773cc2d028d0e084719
SATYA AMAN <satyaaman1012@gmail.com>
2025-09-11 00:30:54 +0530

    added analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     return (
         <div className="min-h-screen">
             <Home />
+            <Analytics />
         </div>
     );
 }
